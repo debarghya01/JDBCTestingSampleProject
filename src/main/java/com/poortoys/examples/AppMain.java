da4057fcc517605518660e987140aac6e213a852
debarghya01 <hara.deb@gmail.com>
2021-03-22 07:46:46 -0400

    Added insert & update

--- src/main/java/com/poortoys/examples/AppMain.java
+++ src/main/java/com/poortoys/examples/AppMain.java
@@ -13,9 +13,11 @@ public class AppMain {
         }catch(Exception e) {
         	
         }
+//		Try with resource, so no need to explicitly close the connection  
         try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc_test?useSLL=flase","root","root");
         		Statement stmt = conn.createStatement();){
-        	
+
+//			Reading from table
         	String query = "select ID, NAME, SAL from emp_table;";
         	ResultSet rs = stmt.executeQuery(query);
         	
@@ -28,9 +30,26 @@ public class AppMain {
         		
         	}
         	
+//			Inserting into table
+        	long insertID = 4;
+        	String insertName = "Test User4";
+        	double insertSalary = 10000;
+        	
+        	String insertQuery = "Insert into emp_table values(" + insertID + ",'" + insertName + "'," + insertSalary + ")";
+        	int insertCount = stmt.executeUpdate(insertQuery);
+        	System.out.println("Number of rows inserted " + insertCount);
+        	
+//			Updating table row
+        	long updateID = 4;
+        	long updateSalary = 60000;
+        	
+        	String updateQuery = "update emp_table set sal = " + updateSalary + " where ID = " + updateID ;
+        	int updateCount = stmt.executeUpdate(updateQuery);
+        	System.out.println("Number of rows updated " + updateCount);
+        	
         			
         }catch(SQLException e) {
-        	
+        	System.out.println(e);
         }
         	  
    	}
